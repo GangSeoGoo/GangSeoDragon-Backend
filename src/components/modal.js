81aa68aa6fff10fbf2a042941228728216b0484d
jeongdonggyeom <topcome928@gmail.com>
2022-07-07 10:05:31 +0900

    팝업창 수정

--- src/components/modal.js
+++ src/components/modal.js
@@ -4,13 +4,15 @@ import '../styleComponents/modal.css';
 const Modal = (props) => {
     
     const review = props.review;
+    console.log(review);
     
     useEffect(()=>{
         document.querySelector('.modal').innerHTML = "";
+        document.querySelector('.modal').innerHTML = `<p>${review[0].tourName}</p><img src=${review[0].tourImage} alt="icon"/>`;
         for(let i=0;i<review.length;i++){
-            document.querySelector('.modal').innerHTML += `<p>${review[i].tourName}</p><img src=${review[i].tourImage} alt="icon"/><p>${review[i].reviewStar}</p><p>${review[i].reviewText}</p>`
+            document.querySelector('.modal').innerHTML += `<p>${review[i].reviewStar}</p><p>${review[i].reviewText}</p>`
         }
-    }, [])
+    }, [review]);
     
 
     return(
